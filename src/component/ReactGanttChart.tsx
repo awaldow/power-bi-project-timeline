8a964b56c41b290f1caee4778ea4ee8fedd2ba14
Addison Waldow <a.wal.bear@gmail.com>
2020-06-10 12:09:43 -0700

    get a little further in the tutorial

--- src/component/ReactGanttChart.tsx
+++ src/component/ReactGanttChart.tsx
@@ -2,9 +2,54 @@
 
 import * as React from "react";
 
-export class ReactGanttChart extends React.Component<{}> {
+export interface State {
+  size: number;
+  textLabel: string;
+  textValue: string;
+}
+
+export const initialState: State = {
+  textLabel: "",
+  textValue: "",
+  size: 200,
+};
+
+export class ReactGanttChart extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = initialState;
+  }
+
+  private static updateCallback: (data: object) => void = null;
+
+  public static update(newState: State) {
+    if (typeof ReactGanttChart.updateCallback === "function") {
+      ReactGanttChart.updateCallback(newState);
+    }
+  }
+
+  public state: State = initialState;
+
+  public componentWillMount() {
+    ReactGanttChart.updateCallback = (newState: State): void => {
+      this.setState(newState);
+    };
+  }
+
+  public componentWillUnmount() {
+    ReactGanttChart.updateCallback = null;
+  }
+
   render() {
-    return <div className="circleCard">Hello, React!</div>;
+    const { textLabel, textValue, size } = this.state;
+
+    const style: React.CSSProperties = { width: size, height: size };
+
+    return (
+      <div className="circleCard" style={style}>
+        {/* ... */}
+      </div>
+    );
   }
 }
 
--- src/visual.ts
+++ src/visual.ts
@@ -7,12 +7,14 @@ import DataView = powerbi.DataView;
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbi.extensibility.visual.IVisual;
+import IViewport = powerbi.IViewport;
 
 import "./../style/visual.less";
 
-import ReactGanttChart from "./component/ReactGanttChart";
+import { ReactGanttChart, initialState } from "./component/ReactGanttChart";
 
 export class Visual implements IVisual {
+  private viewport: IViewport;
   private target: HTMLElement;
   private reactRoot: React.ComponentElement<any, any>;
 
@@ -23,5 +25,25 @@ export class Visual implements IVisual {
     ReactDOM.render(this.reactRoot, this.target);
   }
 
-  public update(options: VisualUpdateOptions) {}
+  public update(options: VisualUpdateOptions) {
+    if (options.dataViews && options.dataViews[0]) {
+      const dataView: DataView = options.dataViews[0];
+
+      this.viewport = options.viewport;
+      const { width, height } = this.viewport;
+      const size = Math.min(width, height);
+
+      ReactGanttChart.update({
+        size,
+        textLabel: dataView.metadata.columns[0].displayName,
+        textValue: dataView.single.value.toString(),
+      });
+    } else {
+      this.clear();
+    }
+  }
+
+  private clear() {
+    ReactGanttChart.update(initialState);
+  }
 }
