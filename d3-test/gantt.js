11cfa26f75638f1322cb5f114aee91c95b71e844
Addison Waldow <a.wal.bear@gmail.com>
2020-07-06 16:23:11 -0700

    remove some comments

--- d3-test/gantt.js
+++ d3-test/gantt.js
@@ -12,20 +12,17 @@ d3.gantt = function () {
   var timeDomainEnd = d3.timeMonth.offset(new Date(), -14);
   var timeDomainMode = FIT_TIME_DOMAIN_MODE;// fixed or fit
   var projects = [];
-  var taskStatus = [];
   var height = document.body.clientHeight - margin.top - margin.bottom - 5;
   var width = document.body.clientWidth - margin.right - margin.left - 5;
 
   var tickFormat = "%m/%d/%Y";
 
   var keyFunction = function (d) {
-    //return d.startDate + d.taskName + d.endDate;
     return d.pmAssignDate + d.projectName + d.endDate;
   };
 
   var rectTransform = function (d) {
     return "translate(" + x(d.pmAssignDate) + "," + y(d.projectName) + ")";
-    //return "translate(" + x(d.pmAssignDate) + "," + 20 + ")";
   };
 
   var x, y, xAxis, yAxis;
@@ -56,7 +53,6 @@ d3.gantt = function () {
   function initAxis() {
     x = d3.scaleTime().domain([timeDomainStart, timeDomainEnd]).range([0, width]).clamp(true);
 
-    //y = d3.scaleBand().domain(projects.map(p => p.projectName)).rangeRound([0, height - margin.top - margin.bottom], .1);
     y = d3.scaleOrdinal().domain([0, projects.length]).range([0, projects.length * 20]);
 
     xAxis = d3.axisTop().scale(x).tickFormat(d3.timeFormat(tickFormat))
